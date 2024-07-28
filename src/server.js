01385ada989f2b0b9b3b935835ed379d4337961f
ingalejay1 <ingalejay1@gmail.com>
2024-07-28 21:30:50 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -5,10 +5,17 @@ dotenv.config({
     path: './env'
 })
 
-// require('dotenv').config({path: './env'})
-
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port : $
+        {process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
